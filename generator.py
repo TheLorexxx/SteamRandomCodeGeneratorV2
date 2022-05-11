30de9d1e866275f792d69e0c81addb55d27348e9
TheLorexxx <104691158+TheLorexxx@users.noreply.github.com>
2022-05-11 20:15:37 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,128 @@
+import time
+import random
+import os
+
+steamkod = ["M" , "6" , "8" , "B" , "H" , "Q" , "C" , "2"  , "I" , "A"  , "K" , "Z" , "5" , "0" , "9" , "7" , "3" , "4"  , "P" , "S" , "R" , "T" , "L"]
+
+steamkod1 = random.choice(steamkod)
+steamkod2 = random.choice(steamkod)
+steamkod3 = random.choice(steamkod)
+steamkod4 = random.choice(steamkod)
+steamkod5 = random.choice(steamkod)
+
+steamkodbosluk = "-"
+
+steamkod6 = random.choice(steamkod)
+steamkod7 = random.choice(steamkod)
+steamkod8 = random.choice(steamkod)
+steamkod9 = random.choice(steamkod)
+
+steamkodbosluk2 = "-"
+
+steamkod10 = random.choice(steamkod)
+steamkod11 = random.choice(steamkod)
+steamkod12 = random.choice(steamkod)
+steamkod13 = random.choice(steamkod)
+
+# Steam Kod 2
+
+steamkod_a1 = random.choice(steamkod)
+steamkod_a2 = random.choice(steamkod)
+steamkod_a3 = random.choice(steamkod)
+steamkod_a4 = random.choice(steamkod)
+steamkod_a5 = random.choice(steamkod)
+
+steamkod_abosluk = "-"
+
+steamkod_a6 = random.choice(steamkod)
+steamkod_a7 = random.choice(steamkod)
+steamkod_a8 = random.choice(steamkod)
+steamkod_a9 = random.choice(steamkod)
+
+steamkod_abosluk2 = "-"
+
+steamkod_a10 = random.choice(steamkod)
+steamkod_a11 = random.choice(steamkod)
+steamkod_a12 = random.choice(steamkod)
+steamkod_a13 = random.choice(steamkod)
+
+# Steam Kod 3
+
+
+steamkod_b1 = random.choice(steamkod)
+steamkod_b2 = random.choice(steamkod)
+steamkod_b3 = random.choice(steamkod)
+steamkod_b4 = random.choice(steamkod)
+steamkod_b5 = random.choice(steamkod)
+
+steamkod_bbosluk = "-"
+
+steamkod_b6 = random.choice(steamkod)
+steamkod_b7 = random.choice(steamkod)
+steamkod_b8 = random.choice(steamkod)
+steamkod_b9 = random.choice(steamkod)
+
+steamkod_bbosluk2 = "-"
+
+steamkod_b10 = random.choice(steamkod)
+steamkod_b11 = random.choice(steamkod)
+steamkod_b12 = random.choice(steamkod)
+steamkod_b13 = random.choice(steamkod)
+
+# Steam Kod 4
+
+steamkod_c1 = random.choice(steamkod)
+steamkod_c2 = random.choice(steamkod)
+steamkod_c3 = random.choice(steamkod)
+steamkod_c4 = random.choice(steamkod)
+steamkod_c5 = random.choice(steamkod)
+
+steamkod_cbosluk = "-"
+
+steamkod_c6 = random.choice(steamkod)
+steamkod_c7 = random.choice(steamkod)
+steamkod_c8 = random.choice(steamkod)
+steamkod_c9 = random.choice(steamkod)
+
+steamkod_cbosluk2 = "-"
+
+steamkod_c10 = random.choice(steamkod)
+steamkod_c11 = random.choice(steamkod)
+steamkod_c12 = random.choice(steamkod)
+steamkod_c13 = random.choice(steamkod)
+print("Bu program Lorex tarafindan yapilmistir.")
+print("Devam etmek icin herhangi bir  tusa tiklayiniz.")
+
+os.system("color a")
+os.system("pause > ProgramimiziKullandiginizIcinTesekkurEderiz_ByLorex")
+os.system("cls")
+print("Steam Kod Olusturuluyor.")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuluyor..")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuluyor...")
+time.sleep(1)
+os.system("cls")
+print("Steam Kod Olusturuldu!")
+time.sleep(1)
+os.system("cls")
+print("Ekrana yazdiriliyor.")
+time.sleep(1)
+os.system("cls")
+print("Ekrana yazdiriliyor..")
+time.sleep(1)
+os.system("cls")
+print("Ekrana yazdiriliyor...")
+time.sleep(1)
+os.system("cls")
+print(steamkod1+steamkod2+steamkod3+steamkod4+steamkod5+steamkodbosluk+steamkod6+steamkod7+steamkod8+steamkod9+steamkodbosluk2+steamkod10+steamkod11+steamkod12+steamkod13)
+
+print(steamkod_a1+steamkod_a2+steamkod_a3+steamkod_a4+steamkod_a5+steamkod_abosluk+steamkod_a6+steamkod_a7+steamkod_a8+steamkod_a9+steamkod_abosluk2+steamkod_a10+steamkod_a11+steamkod_a12+steamkod_a13)
+
+print(steamkod_b1+steamkod_b2+steamkod_b3+steamkod_b4+steamkod_b5+steamkod_bbosluk+steamkod_b6+steamkod_b7+steamkod_b8+steamkod_b9+steamkod_bbosluk2+steamkod_b10+steamkod_b11+steamkod_b12+steamkod_b13)
+
+print(steamkod_c1+steamkod_c2+steamkod_c3+steamkod_c4+steamkod_c5+steamkod_cbosluk+steamkod_c6+steamkod_c7+steamkod_c8+steamkod_c9+steamkod_cbosluk2+steamkod_c10+steamkod_c11+steamkod_c12+steamkod_c13)
+
+time.sleep(99)
